f6763baadb70a2d5a596eb7cbb76a5dc22e2a99a
Xusen Li <lixusen@marvell.com>
2022-09-25 00:31:41 +0800

    submit

---
+++ lg-rpc/lg-rpc-provider/src/main/java/com/lagou/rpc/provider/ServerBootstrapApplication.java
@@ -0,0 +1,29 @@
+package com.lagou.rpc.provider;
+
+import com.lagou.rpc.provider.server.RpcServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBootstrapApplication implements CommandLineRunner {
+
+
+    @Autowired
+    RpcServer rpcServer;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerBootstrapApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                rpcServer.startServer("127.0.01", 8899);
+            }
+        }).start();
+    }
+}
